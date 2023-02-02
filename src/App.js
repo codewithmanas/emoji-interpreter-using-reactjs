bc3730c7b24f71fad96afd7568863240f6ec3ff8
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-02-02 19:00:23 +0530

    initial commit
    basic emoji interpreter added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
+import "./App.css";
+
+var color = "#1D4ED8";
+
+const emojiDictionary = {
+  "😊": "Smiling",
+  "😲": "Disbelief",
+  "😞": "Sad",
+  "🥱": "Yawning",
+  "🤑": "Money-Face",
+  "😴": "Sleeping"
+};
+
+export default function App() {
+  // const [userInput, setUserInput] = useState("");
+
+  // function inputChangeHandler(event) {
+  // console.log(event.target);
+  // console.log(event.target.value);
+  //   setUserInput(event.target.value);
+  // }
+  const [userInput, setUserInput] = useState("");
+  function emojiInputHandler(event) {
+    // console.log(event.target.value);
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+
+    console.log(meaning);
+    if (meaning === undefined && userInput !== "") {
+      meaning = "we don't have this in our database";
+    }
+    setUserInput(meaning);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        inside <span style={{ color: color }}>outtt...</span>
+      </h1>
+      <input onChange={emojiInputHandler}></input>
+      {/* <input onChange={inputChangeHandler}></input>
+
+      <div>Welcome {userInput}</div> */}
+      <div>
+        <span style={{ fontWeight: "bold" }}>{userInput}</span>
+      </div>
     </div>
   );
 }
-
-export default App;
